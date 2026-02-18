d8cae6d071eb0cf05a333a8a48a0f5b05991b2b1
Alfian Pasomba <alfianmuhammad105@gmail.com>
2026-02-18 22:36:20 +0800

    membuat API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    serializer_class = serializers.HelloSerializers
+    def get(self, request, format=None):
+        joblist = [
+            "data analyst",
+            "data scientist",
+            "ml engineer",
+            "ai engineer",
+        ]
+
+        return Response({"message":"tes api view pertama kali", "data":joblist})
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Halo {name}"
+            return Response({"message":message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+    def patch(self, request, pk=None):
+        return Response({"method":"PATCH"})
+    
+    def put(self, request, pk=None):
+        return Response({"method":"PUT"})
+    
+    def delete(self, request, pk=None):
+        return Response({"method":"DELETE"})
